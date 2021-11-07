60f1f791c9e8a59143b46bde3086ae5ca3e99976
Wilton Leonardo <wilton.leonardo@hotmail.com>
2021-11-06 23:37:52 -0300

    wip: Inserting in trie correctly

--- Makefile
+++ Makefile
@@ -7,8 +7,8 @@ run-test:
 	rm -f arvore
 	gcc manipula.c -o program -lm
 	./program < ./in/1.in > 1-p.out
-	cmp ./out/1.out 1-p.out
-	rm 1-p.out
+	# cmp ./out/1.out 1-p.out
+	# rm 1-p.out
 
 run-test-2:
 	rm -f dados
--- manipula.c
+++ manipula.c
@@ -2,27 +2,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include "queue.h"
 
-#define GRAUMINIMO 2
-#define MIN_NO GRAUMINIMO - 1
-#define MAX_NO 2 * (GRAUMINIMO)-1
-#define END_POINT 4 * (GRAUMINIMO)-1
-
+#define MAXNSIMBOLOS 5
 typedef struct {
-    int chave;
-    char nome[20];
-    int idade;
+    char palavra[30];
+    int frequencia;
 } RegistroArquivoDados;
 
 typedef struct {
-    int chave;
-    int apontador;
-} No;
-
-typedef struct {
-    int ocupados;
-    No nos[END_POINT];
+  char letra[1];
+  int dados;
+  int esquerda;
+  int direita;
 } RegistroArquivoArvore;
 
 typedef struct {
@@ -66,13 +57,11 @@ void abreArquivo(FILE **dados, FILE **arvore, Controle *c) {
 }
 
 void setRaiz(Controle *c, FILE **arvore) {
-  if (c->proximoDadosLivre == 0) {
+  if (c->deslocamentoRaiz == -1) {
     RegistroArquivoArvore raiz;
-    raiz.ocupados = 0;
-    for (int i = 0; i < END_POINT; i++) {
-      raiz.nos[i].apontador = -1;
-      raiz.nos[i].chave = -1;
-    }
+    raiz.letra[0] = '*';
+    raiz.esquerda = -1;
+    raiz.direita = -1;
 
     fseek(*arvore, sizeof(*c), SEEK_SET);
     if (!fwrite(&raiz, sizeof(raiz), 1, *arvore)) {
@@ -87,213 +76,15 @@ void setRaiz(Controle *c, FILE **arvore) {
 
 void fechaArquivo(FILE **arquivo) { fclose(*arquivo); }
 
-void imprimir(RegistroArquivoDados registro) {
-  printf("chave: %d", registro.chave);
-  printf("\n%s", registro.nome);
-  printf("\n%d", registro.idade);
-  printf("\n");
-}
-
-void splitNode(FILE **arvore, Controle *c, int deslocamentoFilho, int deslocamentoPai, RegistroArquivoArvore *registro) {
-  RegistroArquivoArvore pai;
-  if (deslocamentoPai != -1) {
-    fseek(*arvore, sizeof(*c) + deslocamentoPai * sizeof(pai), SEEK_SET);
-    fread(&pai, sizeof(pai), 1, *arvore);
-  } else {
-    deslocamentoPai = c->proximoArvoreLivre;
-    for (int i = 0; i < END_POINT; i++) {
-      pai.nos[i].apontador = -1;
-      pai.nos[i].chave = -1;
-    }
-    pai.ocupados = 0;
-    c->deslocamentoRaiz = deslocamentoPai;
-    c->proximoArvoreLivre++;
-  }
-
-  RegistroArquivoArvore filho;
-  fseek(*arvore, sizeof(*c) + deslocamentoFilho * sizeof(filho), SEEK_SET);
-  fread(&filho, sizeof(filho), 1, *arvore);
-
-  double maxNo = MAX_NO;
-  int mid = ceil(maxNo / 2) - 1;
-  int deslocamentoNovoFilho = c->proximoArvoreLivre;
-  c->proximoArvoreLivre++;
-
-  No esquerda;
-  esquerda.apontador = deslocamentoFilho;
-  esquerda.chave = -1;
-
-  No direita;
-  direita.apontador = deslocamentoNovoFilho;
-  direita.chave = -1;
-
-  int i = (pai.ocupados * 2) - 1;
-  int z = i;
-  while (z >= 1) {
-    if (filho.nos[(mid * 2) + 1].chave > pai.nos[z].chave) {
-      break;
-    }
-    z -= 2;
-    i = z;
-  }
-
-  No nomedio = filho.nos[(mid * 2) + 1];
-  int chave;
-  if (i == (pai.ocupados * 2) - 1) {
-    // Novo maior numero
-    chave = i + 2;
-  } else {
-    // Está no meio ou novo menor numero
-    for (int j = (pai.ocupados * 2) - 1; j >= i; j -= 2) {
-      if(j == (pai.ocupados * 2) - 1) {
-        pai.nos[j + 3] = pai.nos[j + 1];
-      }
-      pai.nos[j + 2] = pai.nos[j];
-      pai.nos[j + 1] = pai.nos[j - 1];
-    }
-    chave = (i < 0) ? 1 : i;
-  }
-
-  pai.nos[chave] = nomedio;
-  pai.nos[chave - 1] = esquerda;
-  pai.nos[chave + 1] = direita;
-  pai.ocupados++;
-
-  RegistroArquivoArvore novoFilho;
-  novoFilho.ocupados = 0;
-  for (int i = 0; i < END_POINT; i++) {
-    novoFilho.nos[i].apontador = -1;
-    novoFilho.nos[i].chave = -1;
-  }
-
-  int index = 0;
-  for (int i = (mid + 1) * 2; i < END_POINT; i++) {
-    novoFilho.nos[index] = filho.nos[i];
-    if (filho.nos[i].chave != -1) {
-      novoFilho.ocupados++;
-    }
-    index++;
-  }
-
-  for (int i = (mid * 2) + 1; i < END_POINT; i++) {
-    if(filho.nos[i].chave != -1) {
-      filho.ocupados--;
-    }
-
-    filho.nos[i].apontador = -1;
-    filho.nos[i].chave = -1;
-  }
-
-  // Atualizando pai na memória
-  fseek(*arvore, sizeof(*c) + deslocamentoPai * sizeof(pai), SEEK_SET);
-  fwrite(&pai, sizeof(pai), 1, *arvore);
-
-  // Atualizando filho na memória
-  fseek(*arvore, sizeof(*c) + deslocamentoFilho * sizeof(filho), SEEK_SET);
-  fwrite(&filho, sizeof(filho), 1, *arvore);
-
-  // Criando novo filho na memória
-  fseek(*arvore, sizeof(*c) + deslocamentoNovoFilho * sizeof(novoFilho), SEEK_SET);
-  fwrite(&novoFilho, sizeof(novoFilho), 1, *arvore);
-
-  // Copiando informação do pai para o registro corrente
-  for (int i = 0; i < END_POINT; i++) {
-    registro->nos[i] = pai.nos[i];
-  }
-  registro->ocupados = pai.ocupados;
-}
-
 int buscaChaveCadastro(int chave, FILE **arvore, Controle *c) {
   RegistroArquivoArvore registro;
   fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(registro), SEEK_SET);
   fread(&registro, sizeof(registro), 1, *arvore);
 
-  if (registro.ocupados == 0) {
-    registro.nos[1].chave = chave;
-    registro.nos[1].apontador = c->proximoDadosLivre;
-    registro.ocupados++;
-    fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(registro), SEEK_SET);
-    fwrite(&registro, sizeof(registro), 1, *arvore);
-    return 0;
-  }
+  fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(registro), SEEK_SET);
+  fwrite(&registro, sizeof(registro), 1, *arvore);
 
-  int deslocamento = -2;
-  int deslocamentoPai = -1;
-  int noAtual = c->deslocamentoRaiz;
-  while (deslocamento == -2) {
-    if (registro.ocupados == MAX_NO) {
-      splitNode(arvore, c, noAtual, deslocamentoPai, &registro);
-    }
-
-    int indice1 = (registro.ocupados * 2) - 1;
-    int z = indice1;
-    while (z >= 1) {
-      if (registro.nos[z].chave <= chave) {
-        break;
-      }
-      z -= 2;
-      indice1 = z;
-    }
-
-    if (indice1 < 0) {
-      indice1 = 1;
-    }
-
-    if (registro.nos[indice1].chave == chave) {
-      deslocamento = -1;
-      continue;
-    }
-
-    int proximoNo = registro.nos[indice1 + 1].apontador;
-    if (registro.nos[indice1].chave > chave) {
-      proximoNo = registro.nos[indice1 - 1].apontador;
-    }
-
-    if (proximoNo == -1) {
-      deslocamento = c->proximoDadosLivre;
-
-      int indice2 = (registro.ocupados * 2) - 1;
-      int x = indice2;
-      while (x >= 1) {
-        if (registro.nos[x].chave < chave) {
-          break;
-        }
-        x -= 2;
-        indice2 = x;
-      }
-
-      int indice;
-      if (indice2 == (registro.ocupados * 2) - 1) {
-        // Novo maior numero
-        indice = indice2 + 2;
-      } else {
-        // Está no meio ou é o novo menor número
-        for (int indice4 = (registro.ocupados * 2) - 1; indice4 > indice2; indice4 -= 2) {
-          if(indice4 == (registro.ocupados * 2) - 1) {
-            registro.nos[indice4 + 3] = registro.nos[indice4 + 1];
-          }
-          registro.nos[indice4 + 2] = registro.nos[indice4];
-          registro.nos[indice4 + 1] = registro.nos[indice4 - 1];
-        }
-        indice = (indice2 < 0) ? 1 : indice2 + 2;
-      }
-
-      No novoNo;
-      novoNo.apontador = deslocamento;
-      novoNo.chave = chave;
-      registro.nos[indice] = novoNo;
-      registro.ocupados++;
-
-      fseek(*arvore, sizeof(*c) + noAtual * sizeof(registro), SEEK_SET);
-      fwrite(&registro, sizeof(registro), 1, *arvore);
-      continue;
-    }
-
-    deslocamentoPai = noAtual;
-    noAtual = proximoNo;
-    fseek(*arvore, sizeof(*c) + proximoNo * sizeof(registro), SEEK_SET);
-    fread(&registro, sizeof(registro), 1, *arvore);
-  }
+  int deslocamento = 0;
 
   return deslocamento;
 }
@@ -303,77 +94,128 @@ int buscaChaveConsulta(int chave, FILE **arvore, Controle *c) {
   fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(registro), SEEK_SET);
   fread(&registro, sizeof(registro), 1, *arvore);
 
-  if (registro.ocupados == 0) {
-    return -1;
-  }
-
-  int deslocamento = -2;
-  int deslocamentoPai = -1;
-  int noAtual = c->deslocamentoRaiz;
-  while (deslocamento == -2) {
-    if (registro.ocupados == MAX_NO) {
-      splitNode(arvore, c, noAtual, deslocamentoPai, &registro);
-    }
-
-    int i = (registro.ocupados * 2) - 1;
-    int z = i;
-    while (z >= 1) {
-      if (registro.nos[z].chave <= chave) {
-        i = z;
-        break;
-      }
-      z -= 2;
-    }
-
-    if (i < 0) {
-      i = 1;
-    }
-
-    if (registro.nos[i].chave == chave) {
-      deslocamento = registro.nos[i].apontador;
-      continue;
-    }
-
-    int proximoNo = registro.nos[i + 1].apontador;
-    if (registro.nos[i].chave > chave) {
-      proximoNo = registro.nos[i - 1].apontador;
-    }
-
-    if (proximoNo == -1) {
-      deslocamento = -1;
-      continue;
-    }
-
-    deslocamentoPai = noAtual;
-    noAtual = proximoNo;
-    fseek(*arvore, sizeof(*c) + proximoNo * sizeof(registro), SEEK_SET);
-    fread(&registro, sizeof(registro), 1, *arvore);
-  }
+  int deslocamento = 0;
 
   return deslocamento;
 }
 
 void cadastrar(FILE **dados, FILE **arvore, Controle *c) {
-  RegistroArquivoDados r;
-  scanf("%d%*c", &r.chave);
+  int linhas;
+  scanf("%d%*c", &linhas);
 
-  fgets(r.nome, 20, stdin);
-  // Limpando enter final do buffer
-  size_t ln = strlen(r.nome) - 1;
-  if (r.nome[ln] == '\n') r.nome[ln] = '\0';
+  char palavrasEntrada[linhas][30];
 
-  scanf("%d%*c", &r.idade);
-
-  int deslocamento = buscaChaveCadastro(r.chave, arvore, c);
-  if (deslocamento == -1) {
-    printf("chave ja existente: %d\n", r.chave);
-    return;
+  for (int i = 0; i < linhas; i++) {
+    fgets(palavrasEntrada[i], 30, stdin);
+    size_t ln = strlen(palavrasEntrada[i]) - 1;
+    if (palavrasEntrada[i][ln] == '\n') 
+      palavrasEntrada[i][ln] = '\0';
   }
 
-  fseek(*dados, deslocamento * sizeof(r), SEEK_SET);
-  if (fwrite(&r, sizeof(r), 1, *dados)) {
-    c->proximoDadosLivre++;
-    printf("insercao com sucesso: %d\n", r.chave);
+  for (int i = 0; i < linhas; i++) {
+    RegistroArquivoArvore raiz;
+    fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(raiz), SEEK_SET);
+    fread(&raiz, sizeof(raiz), 1, *arvore); 
+
+    char palavraCorrente[30];
+    strcpy(palavraCorrente, palavrasEntrada[i]);
+
+    int length = strlen(palavrasEntrada[i]);
+
+    int i = 0;
+    int found = 1;
+    int pai = c->deslocamentoRaiz;
+    RegistroArquivoArvore registro = raiz;
+
+    while (i != length) {
+      if (registro.letra[0] == '*') {
+        registro.letra[0] = palavraCorrente[i];
+        registro.letra[1] = '\0';
+
+        pai = c->deslocamentoRaiz;
+        fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(registro), SEEK_SET);
+        fwrite(&registro, sizeof(registro), 1, *arvore);
+        found = 0;
+      }
+
+      if (registro.letra[0] == '+') {
+        registro.letra[0] = palavraCorrente[i];
+        registro.letra[1] = '\0';
+        fseek(*arvore, sizeof(*c) + pai * sizeof(registro), SEEK_SET);
+        fwrite(&registro, sizeof(registro), 1, *arvore);
+        found = 0;
+      }
+
+      if (registro.letra[0] == palavraCorrente[i]) {
+        if (registro.esquerda == -1) {
+          RegistroArquivoArvore novoRegistro;
+          novoRegistro.letra[0] = '+';
+          registro.letra[1] = '\0';
+          novoRegistro.esquerda = -1;
+          novoRegistro.direita = -1;
+          fseek(*arvore, sizeof(*c) + c->proximoArvoreLivre * sizeof(novoRegistro), SEEK_SET);
+          fwrite(&novoRegistro, sizeof(novoRegistro), 1, *arvore);
+
+          registro.esquerda = c->proximoArvoreLivre;
+          fseek(*arvore, sizeof(*c) + pai * sizeof(registro), SEEK_SET);
+          fwrite(&registro, sizeof(registro), 1, *arvore);
+
+          pai = c->proximoArvoreLivre;
+          c->proximoArvoreLivre++;
+          registro = novoRegistro;
+          found = 0;
+          i++;
+          continue;
+        } else {
+          pai = registro.esquerda;
+          i++;
+          fseek(*arvore, sizeof(*c) + registro.esquerda * sizeof(registro), SEEK_SET);
+          fread(&registro, sizeof(registro), 1, *arvore);
+        }
+      } else {
+        if (registro.direita == -1) {
+          RegistroArquivoArvore novoRegistro;
+          novoRegistro.letra[0] = '+';
+          registro.letra[1] = '\0';
+          novoRegistro.esquerda = -1;
+          novoRegistro.direita = -1;
+          fseek(*arvore, sizeof(*c) + c->proximoArvoreLivre * sizeof(novoRegistro), SEEK_SET);
+          fwrite(&novoRegistro, sizeof(novoRegistro), 1, *arvore);
+
+          registro.direita = c->proximoArvoreLivre;
+          fseek(*arvore, sizeof(*c) + pai * sizeof(registro), SEEK_SET);
+          fwrite(&registro, sizeof(registro), 1, *arvore);
+
+          pai = c->proximoArvoreLivre;
+          c->proximoArvoreLivre++;
+          registro = novoRegistro;
+          found = 0;
+          continue;
+        } else {
+          pai = registro.direita;
+          fseek(*arvore, sizeof(*c) + registro.direita * sizeof(registro), SEEK_SET);
+          fread(&registro, sizeof(registro), 1, *arvore);
+        }
+      }
+    }
+
+    if (found == 0) {
+      RegistroArquivoDados dadosPalavra;
+      dadosPalavra.frequencia = 0;
+      strcpy(dadosPalavra.palavra, palavraCorrente);
+
+      if (registro.letra[0] == '+') {
+        registro.letra[0] = '=';
+        registro.letra[1] = '\0';
+        registro.dados = c->proximoDadosLivre;
+        fseek(*arvore, sizeof(*c) + pai * sizeof(registro), SEEK_SET);
+        fwrite(&registro, sizeof(registro), 1, *arvore);
+      }
+
+      fseek(*dados, c->proximoDadosLivre * sizeof(dadosPalavra), SEEK_SET);
+      fwrite(&dadosPalavra, sizeof(dadosPalavra), 1, *dados);
+      c->proximoDadosLivre++;
+    }
   }
 }
 
@@ -390,112 +232,26 @@ void consultar(FILE **dados, FILE **arvore, Controle *c) {
   RegistroArquivoDados r;
   fseek(*dados, deslocamento * sizeof(r), SEEK_SET);
   fread(&r, sizeof(r), 1, *dados);
-  imprimir(r);
 }
 
-void imprimeNo(int deslocamento, FILE **arvore){
-  RegistroArquivoArvore no;
+void printNo (int deslocamento, FILE **arvore) {
+  RegistroArquivoArvore registro;
+  fseek(*arvore, sizeof(Controle) + deslocamento * sizeof(registro), SEEK_SET);
+  fread(&registro, sizeof(registro), 1, *arvore); 
 
-  fseek(*arvore, sizeof(Controle) + deslocamento * sizeof(no), SEEK_SET);
-  fread(&no, sizeof(no), 1, *arvore);
+  printf("\ndesl: %d no: %s esq: %d dir: %d", deslocamento, registro.letra, registro.esquerda, registro.direita);
 
-  for (int i = 0; i < END_POINT; i++) {
-    if (no.nos[i].apontador != -1 && no.nos[i].chave == -1) {
-      imprimeNo(no.nos[i].apontador, arvore);
-      continue;
-    }
-    
-    if (no.nos[i].chave != -1) {
-      printf("%d\n", no.nos[i].chave);
-    }
+  if (registro.esquerda != -1){
+    printNo(registro.esquerda, arvore);
+  }
+
+  if (registro.direita != -1){
+    printNo(registro.direita, arvore);
   }
 }
 
-int imprimeNoLargura(int index, int offset, int deslocamento, Queue *queue, FILE **arvore) {
-RegistroArquivoArvore no;
-
-  fseek(*arvore, sizeof(Controle) + deslocamento * sizeof(no), SEEK_SET);
-  fread(&no, sizeof(no), 1, *arvore);
-
-  printf("No: %d:", index);
-  int usados = 0;
-  int apontadores = index != 1 ? index + (offset-1) : index + offset;
-  for (int i = 0; i <= no.ocupados * 2; i++) {
-    if (no.nos[i].chave == -1) {
-      char *apontador = (char*) malloc(100);
-      
-      if (no.nos[i].apontador != -1) {
-        apontadores++;
-        usados++;
-        snprintf(apontador, 100, "%d", apontadores);
-        enqueue(queue, no.nos[i].apontador);
-      } else {
-        strcpy(apontador, "null");
-      }
-
-      printf(" apontador: %s", apontador);
-      free(apontador);
-    }
-
-    if (no.nos[i].chave != -1) {
-      printf(" chave: %d", no.nos[i].chave);
-    }
-  }
-  printf("\n");
-  return usados;
-}
-
-void imprimeArvore(FILE **arvore, Controle *c) {
-  Queue* queue = createQueue(c->proximoArvoreLivre+1);
-  enqueue(queue, c->deslocamentoRaiz);
-
-  int index = 1;
-  int usados = 0;
-  while (!isEmpty(queue)) {
-    int deslocamento = dequeue(queue);
-
-    int tst = 0;
-    if (deslocamento != c->deslocamentoRaiz) {
-      tst = 1;
-    }
-
-    usados = imprimeNoLargura(index, usados, deslocamento, queue, arvore) + tst;
-    index++;
-  }
-}
-
-void imprimeOrdem(FILE **arvore, Controle *c) {
-  RegistroArquivoArvore arvoreRaiz;
-
-  fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(arvoreRaiz), SEEK_SET);
-  fread(&arvoreRaiz, sizeof(arvoreRaiz), 1, *arvore);
-
-  for (int i = 0; i < END_POINT; i++) {
-    if (arvoreRaiz.nos[i].apontador != -1 && arvoreRaiz.nos[i].chave == -1) {
-      imprimeNo(arvoreRaiz.nos[i].apontador, arvore);
-      continue;
-    }
-
-    if (arvoreRaiz.nos[i].chave != -1) {
-      printf("%d\n", arvoreRaiz.nos[i].chave);
-    }
-  }
-}
-
-void taxaDeOcupacao(FILE **arvore, Controle *c) {
-  RegistroArquivoArvore arvoreRaiz;
-
-  fseek(*arvore, sizeof(*c) + c->deslocamentoRaiz * sizeof(arvoreRaiz), SEEK_SET);
-  fread(&arvoreRaiz, sizeof(arvore), 1, *arvore);
-
-  if (arvoreRaiz.ocupados == 0) {
-    printf("árvore vazia\n");
-    return;
-  }
-
-  float qntRegistros = (float) c->proximoDadosLivre;
-  float qntNos = (float) c->proximoArvoreLivre;
-  printf("taxa de ocupacao: %.1f\n", qntRegistros / (qntNos * MAX_NO));
+void imprimeArvore(FILE **dados, FILE **arvore, Controle *c) {
+  printNo(c->deslocamentoRaiz, arvore);
 }
 
 int main(void) {
@@ -522,29 +278,27 @@ int main(void) {
       case 'e': {
         break;
       }
-      case 'c': {
-        // Deve consultar a chave
-        consultar(&pont_dados, &pont_arvore, &controle);
-        break;
-      }
       case 'i': {
-        // Deve inserir o registro
+        // Deve inserir a palavra
         cadastrar(&pont_dados, &pont_arvore, &controle);
         break;
       }
-      case 'p': {
-        // Deve imprimir a arvore
-        imprimeArvore(&pont_arvore, &controle);
+      case 'd': {
+        // Deve consultar a palavra
+        consultar(&pont_dados, &pont_arvore, &controle);
         break;
       }
-      case 'o': {
-        // Deve imprimir as chaves em ordem crescente
-        imprimeOrdem(&pont_arvore, &controle);
+      case 'f': {
+        // Deve imprimir as palavras em ordem alfabética com frequência
+        break;
+      }
+      case 'p': {
+        // Deve imprimir as próximas palavras mais frequentes
         break;
       }
       case 't': {
-        // Deve imprimir a taxa de ocupação da arvore
-        taxaDeOcupacao(&pont_arvore, &controle);
+        // Deve imprimir a arvore
+        imprimeArvore(&pont_dados, &pont_arvore, &controle);
         break;
       }
       default:
